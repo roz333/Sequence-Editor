c4e9faa7a7bace7b23f5043b6813412e2c8a41ca
Rozana <rozana3343@hotmail.com>
2018-07-10 21:20:09 +0300

    deleted unnecessary files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { SequencesService } from './shared/services/sequences.service';
-import { SequencesComponent } from './sequences/sequences.component';
 import { SequenceLineComponent } from './sequence-line/sequence-line.component';
 import { TooltipModule } from 'ngx-tooltip';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -11,7 +10,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    SequencesComponent,
     SequenceLineComponent
   ],
   imports: [
